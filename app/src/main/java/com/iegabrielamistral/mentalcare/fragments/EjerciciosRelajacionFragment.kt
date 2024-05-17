300c3beae6d07459dfecc2359ef518e7e9e445a6
ADMIN <katerinrivera2007@gmail.com>
2024-05-17 10:51:57 -0500

    vista de ejercicio yoga vista de ejercicios de relajacion declare e inicialice las varibles

--- app/src/main/java/com/iegabrielamistral/mentalcare/fragments/EjerciciosRelajacionFragment.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/fragments/EjerciciosRelajacionFragment.kt
@@ -1,15 +1,37 @@
 package com.iegabrielamistral.mentalcare.fragments
 
-import android.app.Fragment
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 import com.iegabrielamistral.mentalcare.R
 
 
 class EjerciciosRelajacionFragment : Fragment() {
+
+
+    private lateinit var anterior : ImageButton
+    private lateinit var ejerciciosDeRelajacion : TextView
+    private lateinit var cardYoga : CardView
+    private lateinit var imagenYoga : ImageView
+    private lateinit var yoga : TextView
+    private lateinit var descripcionYoga : TextView
+    private lateinit var cardMeditacion : CardView
+    private lateinit var imagenMeditacion : ImageView
+    private lateinit var meditacion : TextView
+    private lateinit var descripcionMeditacion : TextView
+    private lateinit var cardRespiracionProfunda : CardView
+    private lateinit var imagenRespiracionProfunda : ImageView
+    private lateinit var respiracionProfunda : TextView
+    private lateinit var descripcionRespiracionProfunda : TextView
+
+
 
     companion object {
         fun newInstance() = EjerciciosRelajacionFragment()
@@ -23,19 +45,49 @@ class EjerciciosRelajacionFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        anterior = view.findViewById(R.id.anterior)
+        ejerciciosDeRelajacion = view.findViewById(R.id.ejerciciosDeRelajacion)
+        cardYoga = view.findViewById(R.id.cardYoga)
+        imagenYoga = view.findViewById(R.id.imagenYoga)
+        yoga = view.findViewById(R.id.yoga)
+        descripcionYoga = view.findViewById(R.id.descripcionYoga)
+        cardMeditacion = view.findViewById(R.id.cardMeditacion)
+        imagenMeditacion = view.findViewById(R.id.imagenMeditacion)
+        meditacion = view.findViewById(R.id.meditacion)
+        descripcionMeditacion = view.findViewById(R.id.descripcionMeditacion)
+        cardRespiracionProfunda = view.findViewById(R.id.cardRespiracionProfunda)
+        imagenRespiracionProfunda = view.findViewById(R.id.imagenRespiracionProfunda)
+        respiracionProfunda = view.findViewById(R.id.respiracionProfunda)
+        descripcionRespiracionProfunda = view.findViewById(R.id.descripcionRespiracionProfunda)
 
 
 
+        anterior.setOnClickListener{
+            activity?.onBackPressedDispatcher?.onBackPressed()
+        }
 
 
+        yoga.setOnClickListener{
 
+        }
+
+        meditacion.setOnClickListener{
+
+        }
+
+        ejerciciosDeRelajacion.setOnClickListener{
+
+        }
 
 
     }
+
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,3 +96,4 @@ class EjerciciosRelajacionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_ejercicios_relajacion, container, false)
     }
 }
+
---
+++ app/src/main/java/com/iegabrielamistral/mentalcare/fragments/VistaEjercicioFragment.kt
@@ -0,0 +1,56 @@
+package com.iegabrielamistral.mentalcare.fragments
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.iegabrielamistral.mentalcare.R
+
+class VistaEjercicioFragment : Fragment() {
+
+    private lateinit var anterior : ImageButton
+    private lateinit var titulo : TextView
+    private lateinit var tiempo : ProgressBar
+    private lateinit var cardEjercicio : CardView
+    private lateinit var nombreEjercicio : TextView
+    private lateinit var descripcion: TextView
+
+    companion object {
+        fun newInstance() = VistaEjercicioFragment()
+    }
+
+    private val viewModel: VistaEjercicioViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+
+    }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            anterior = view.findViewById(R.id.anterior)
+            titulo = view.findViewById(R.id.titulo)
+            tiempo = view.findViewById(R.id.tiempo)
+            cardEjercicio = view.findViewById(R.id.cardEjercicio)
+            nombreEjercicio = view.findViewById(R.id.nombreEjercicio)
+            descripcion = view.findViewById(R.id.decripcion)
+
+
+        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+        return inflater.inflate(R.layout.fragment_vista_ejercicio, container, false)
+    }
+}
