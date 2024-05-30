94749494171fb6cf3e50309c7e5b94ee4acfb449
ADMIN <vnatera091@gmail.com>
2024-05-30 09:21:23 -0500

    barra de navegacion

--- app/src/main/java/com/iegabrielamistral/mentalcare/MainActivity.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.iegabrielamistral.mentalcare.fragments.HomeFragment
+import com.iegabrielamistral.mentalcare.fragments.PerfilUsuarioFragment
+import com.iegabrielamistral.mentalcare.fragments.RelajacionFragment
+import com.iegabrielamistral.mentalcare.fragments.TestFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bnvView : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+       loadFragment(HomeFragment())
+        bnvView = findViewById(R.id.bnvView) as BottomNavigationView
+        bnvView.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.home ->{
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.test -> {
+                    loadFragment(TestFragment())
+                    true
+                }
+                R.id.RELAJACION ->{
+                    loadFragment(RelajacionFragment())
+                    true
+                }
+                R.id.perfil_usuario ->{
+                    loadFragment(PerfilUsuarioFragment())
+                    true
+                }
+                else ->{
+                    loadFragment(HomeFragment())
+                    true
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,fragment)
+        transaction.commit()
     }
 }
