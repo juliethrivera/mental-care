de8fe2e5aa96d7508bbfc31e07bbcf498ac4c62a
ADMIN <luisalejandroruiz448@gmail.com>
2024-05-30 10:58:16 -0500

    initial

--- app/src/main/java/com/iegabrielamistral/mentalcare/MainActivity.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.iegabrielamistral.mentalcare
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.ViewCompat.*
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class MainActivity : AppCompatActivity()
+    fun onCreate(savedInstanceState: Bundle?) {
+        val onCreate = super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+fun findViewById(main: Int): View {
+    TODO("Not yet implemented")
 }
+
+fun setContentView(activityMain: Int) {
+    TODO("Not yet implemented")
+}
+
+fun enableEdgeToEdge() {
+    TODO("Not yet implemented")
+}
+
+
+
