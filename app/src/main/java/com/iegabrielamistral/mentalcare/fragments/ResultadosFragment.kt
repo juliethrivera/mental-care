28bc06c0762c2fbdb703facfa37e9f98f55be7df
estudiante <katerinrivera2007@gmail.com>
2024-08-29 10:57:31 -0500

    inicie el boton empezar y el iniciar tiempo  se hizo la carpeta de resultados,se añadieron imagenes se modifico las carpetas de yoga,meditacion,respiracion profunda acomode la vista de resultados

--- app/src/main/java/com/iegabrielamistral/mentalcare/fragments/ResultadosFragment.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/fragments/ResultadosFragment.kt
@@ -1,5 +1,6 @@
 package com.iegabrielamistral.mentalcare.fragments
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +8,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.iegabrielamistral.mentalcare.R
 
-class ResultadosFragment : Fragment() {
+class ResultadosFragment(val selecciones: List<Int>) : Fragment() {
 
     private lateinit var anterior: ImageView
-
-    companion object {
-        fun newInstance() = ResultadosFragment()
-    }
+    private lateinit var progresoDepresion : ProgressBar
+    private lateinit var porcentajeDepresion : TextView
+    private lateinit var progresoAnsiedad : ProgressBar
+    private lateinit var porcentajeAnsiedad : TextView
+    private lateinit var progresoAutoestima : ProgressBar
+    private lateinit var porcentajeAutoestima : TextView
+    private lateinit var progresoEstres : ProgressBar
+    private lateinit var porcentajeEstres : TextView
+    private lateinit var progresoHumor : ProgressBar
+    private lateinit var porcentajeHumor : TextView
 
     private val viewModel: ResultadosViewModel by viewModels()
 
@@ -32,10 +41,36 @@ class ResultadosFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_resultados, container, false)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       val anterior : ImageView = view.findViewById(R.id.anterior)
+        anterior = view.findViewById(R.id.anterior)
+        progresoDepresion = view.findViewById(R.id.progresoDepresion)
+        porcentajeDepresion = view.findViewById(R.id.porcentajeDepresion)
+        progresoAnsiedad= view.findViewById(R.id.progresoAnsiedad)
+        porcentajeAnsiedad = view.findViewById(R.id.porcentajeAnsiedad)
+        progresoAutoestima = view.findViewById(R.id.progresoAutoestima)
+        porcentajeAutoestima = view.findViewById(R.id.porcentajeAutoestima)
+        progresoEstres = view.findViewById(R.id.progresoEstres)
+        porcentajeEstres = view.findViewById(R.id.porcentajeEstres)
+        progresoHumor = view.findViewById(R.id.progresoHumor)
+        porcentajeHumor = view.findViewById(R.id.porcentajeHumor)
+
+        porcentajeEstres.text = selecciones[0].toString() + "%"
+        porcentajeAnsiedad.text = selecciones[1].toString() + "%"
+        porcentajeDepresion.text = selecciones[2].toString() + "%"
+        porcentajeHumor.text = selecciones[3].toString() + "%"
+        porcentajeAutoestima.text = selecciones[4].toString() + "%"
+
+
+        progresoEstres.progress = selecciones[0]
+        progresoAnsiedad.progress = selecciones[1]
+        progresoDepresion.progress = selecciones[2]
+        progresoHumor.progress = selecciones[3]
+        progresoAutoestima.progress = selecciones[4]
+
+
 
         anterior.setOnClickListener {
             val testMentalBlankFragment = TestMentalBlankFragment()
--- app/src/main/java/com/iegabrielamistral/mentalcare/fragments/TestMentalBlankFragment.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/fragments/TestMentalBlankFragment.kt
@@ -3,6 +3,7 @@ package com.iegabrielamistral.mentalcare.fragments
 import android.content.Context
 import androidx.fragment.app.viewModels
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,22 +24,22 @@ import java.io.InputStream
 
 class TestMentalBlankFragment : Fragment() {
 
-    private lateinit var anterior : ImageView
+    private lateinit var anterior: ImageView
     private lateinit var testMental: TextView
-    private lateinit var progreso : ProgressBar
-    private lateinit var cantidadPreguntas : TextView
-    private lateinit var pregunta : TextView
-    private lateinit var opciones : RadioGroup
-    private lateinit var opcion_1 : RadioButton
-    private lateinit var opcion_2 : RadioButton
-    private lateinit var opcion_3 : RadioButton
-    private lateinit var opcion_4 : RadioButton
-    private lateinit var opcion_5 : RadioButton
-    private lateinit var siguiente : Button
+    private lateinit var progreso: ProgressBar
+    private lateinit var cantidadPreguntas: TextView
+    private lateinit var pregunta: TextView
+    private lateinit var opciones: RadioGroup
+    private lateinit var opcion_1: RadioButton
+    private lateinit var opcion_2: RadioButton
+    private lateinit var opcion_3: RadioButton
+    private lateinit var opcion_4: RadioButton
+    private lateinit var opcion_5: RadioButton
+    private lateinit var siguiente: Button
 
-    var question : Int = 0
+    var question: Int = 0
 
-    val selecciones = mutableListOf(0,0,0,0,0)
+    val selecciones = mutableListOf(0, 0, 0, 0, 0)
 
     companion object {
         fun newInstance() = TestMentalBlankFragment()
@@ -65,65 +66,87 @@ class TestMentalBlankFragment : Fragment() {
         opciones = view.findViewById(R.id.opciones)
         opcion_1 = view.findViewById(R.id.opcion_1)
         opcion_2 = view.findViewById(R.id.opcion_2)
-        opcion_3= view.findViewById(R.id.opcion_3)
-        opcion_4= view.findViewById(R.id.opcion_4)
-        opcion_5= view.findViewById(R.id.opcion_5)
-        siguiente= view.findViewById(R.id.Siguiente)
+        opcion_3 = view.findViewById(R.id.opcion_3)
+        opcion_4 = view.findViewById(R.id.opcion_4)
+        opcion_5 = view.findViewById(R.id.opcion_5)
+        siguiente = view.findViewById(R.id.Siguiente)
 
 
         val jsonString = readJsonFromRaw(requireContext(), R.raw.preguntas)
 
         val gson = Gson()
 
-        val testMental: TestMental = gson.fromJson(jsonString, object : TypeToken<TestMental>(){}.type)
+        val testMental: TestMental =
+            gson.fromJson(jsonString, object : TypeToken<TestMental>() {}.type)
 
         cargarPregunta(testMental)
 
         siguiente.setOnClickListener {
             question++
-            if (question <= 20){
+            if (question < 20) {
                 obtenerSeleccion()
                 cargarPregunta(testMental)
-            }else{
+            } else {
+                val resultadosFragment = ResultadosFragment(selecciones)
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView, resultadosFragment).commit()
 
             }
         }
         siguiente.isEnabled = false
 
-        opciones.setOnCheckedChangeListener{ group, checkedId ->
+
+
+        opciones.setOnCheckedChangeListener { group, checkedId ->
             siguiente.isEnabled = checkedId != -1
 
         }
     }
 
     private fun readJsonFromRaw(requireContext: Context, preguntas: Int): String {
-        val inputStream : InputStream = requireContext.resources.openRawResource(preguntas)
+        val inputStream: InputStream = requireContext.resources.openRawResource(preguntas)
         return inputStream.bufferedReader().use { it.readText() }
     }
 
 
     fun obtenerSeleccion() {
 
-        when(opciones.checkedRadioButtonId){
-            opcion_1.id ->{
-                selecciones[0] += 1
+        val categoria = if (question >= 0 && question <= 3) {
+            0
+        } else if (question >= 4 && question <= 7) {
+            1
+        } else if (question >= 8 && question <= 11) {
+            2
+        } else if (question >= 12 && question <= 15) {
+            3
+        } else {
+            4
+        }
+
+        when (opciones.checkedRadioButtonId) {
+            opcion_1.id -> {
+                selecciones[categoria] += 0
             }
-            opcion_2.id ->{
-                selecciones[1] += 1
+
+            opcion_2.id -> {
+                selecciones[categoria] += 10
             }
-            opcion_3.id ->{
-                selecciones[2] += 1
+
+            opcion_3.id -> {
+                selecciones[categoria] += 15
             }
-            opcion_4.id ->{
-                selecciones[3] += 1
+
+            opcion_4.id -> {
+                selecciones[categoria] += 20
             }
-            opcion_5.id ->{
-                selecciones[4] += 1
+
+            opcion_5.id -> {
+                selecciones[4] += 25
             }
         }
     }
 
-    fun cargarPregunta(testMental : TestMental){
+    fun cargarPregunta(testMental: TestMental) {
 
         opciones.clearCheck()
 
@@ -141,12 +164,10 @@ class TestMentalBlankFragment : Fragment() {
 
     }
 
-    fun actualizarProgreso(pregunta: Int){
+    fun actualizarProgreso(pregunta: Int) {
         val p = 5 * pregunta
         progreso.progress = p
     }
-
-
 
 
 }
