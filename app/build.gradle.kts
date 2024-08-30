fdcd0d5f19735d0be066df408e5008c19202b505
estudiante <katerinrivera2007@gmail.com>
2024-08-30 10:57:52 -0500

    inicie el boton empezar y el iniciar tiempo  se hizo la carpeta de resultados,se añadieron imagenes se modifico las carpetas de yoga,meditacion,respiracion profunda acomode la vista de resultados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,6 @@ dependencies {
     // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation ("com.google.code.gson:gson:2.11.0")
 }
