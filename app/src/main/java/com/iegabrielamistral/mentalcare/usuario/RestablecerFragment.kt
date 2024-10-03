ade877b9f8d35bedb80aa236365dfb90a2145063
estudiante <vnatera091@gmail.com>
2024-10-03 08:20:32 -0500

    hacer carpeta de RestablecerFragment y otros tratar de acomodar el bottomNavigation en el LoginActivity que funcione password colocarle margen a los botones y eliminar el bottom de facebook

--- app/src/main/java/com/iegabrielamistral/mentalcare/usuario/RestablecerFragment.kt
+++ app/src/main/java/com/iegabrielamistral/mentalcare/usuario/RestablecerFragment.kt
@@ -20,6 +20,13 @@ class RestablecerFragment : Fragment() {
     lateinit var btnResetPassaword: Button
     private lateinit var auth: FirebaseAuth
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_restablecer, container, false)
+    }
+
     companion object {
         fun newInstance() = RestablecerFragment()
     }
@@ -48,10 +55,5 @@ class RestablecerFragment : Fragment() {
 
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_restablecer, container, false)
-    }
+
 }
