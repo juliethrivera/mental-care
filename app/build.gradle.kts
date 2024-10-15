213c01a7dd5620c6d2c26ddb2cd609c8debebc2c
estudiante <vnatera091@gmail.com>
2024-10-15 10:59:52 -0500

    colocar el bottom de regresar en el fragment_restablecer.xml y hacer que funcione y otras cosas quitarle el color negro al borde de los textInput

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,15 +83,15 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
 
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-auth-ktx")
 
-    // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-database-ktx")
 
 
 }
